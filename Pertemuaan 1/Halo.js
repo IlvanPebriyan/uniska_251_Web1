a4b7440d83cd20d6b2f0050495e5e97aaa9d170b
IlvanPebriyan <ilvanpebriyan17@gmail.com>
2025-12-08 20:02:12 +0800

    Add files via upload

---
+++ Pertemuaan 1/Halo.js
@@ -0,0 +1,16 @@
+     // Variable dan Tipe data
+        var nama = "Ilvan";
+        var umur = 21;
+        var aktif = true;
+        
+        // Operator
+        var x = 10;
+        var y = 5;
+        var hasil = x + y;
+
+        // Kondisi
+        if (umur>18) {
+            alert("Hai" +nama+ "usia Anda sudah Dewasa");
+        } else {
+            alert("Hai" +nama+ "Usia Anda masih dibawah umur");
+        }
---
+++ Pertemuaan 2/Sapa.js
@@ -0,0 +1,4 @@
+function sapa() {
+    var nama = document.getElementById("inputNama").value;
+    alert("halo" + nama + "Selamat datang di halaman profil saya!")
+}
