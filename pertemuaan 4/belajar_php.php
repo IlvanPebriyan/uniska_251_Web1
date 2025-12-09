9c2bbf98caf88655da8733aa2dc06a6c33964a1f
IlvanPebriyan <ilvanpebriyan17@gmail.com>
2025-12-09 17:41:37 +0800

    Add files via upload

---
+++ pertemuaan 4/belajar_php.php
@@ -0,0 +1,27 @@
+<?php
+$a = 9;
+$b = "1";
+$c = $a + $b;
+echo"hellow world!";
+echo "<br>";
+echo $c;
+echo "<br>";
+echo = $d;
+echo "<br>";
+echo =$e;
+$f = ["Hijau", "Kuning","Biru"]
+
+if($d>12) {
+?> 
+<div class="container">
+    <h1>ini kode html</h1>
+</div>
+<?php
+}
+
+echo "<br>";
+echo $e;
+echo $f[0];
+echo "<br>";
+var_dump($f);
+?>
